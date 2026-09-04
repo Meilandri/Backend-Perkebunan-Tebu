3689e09ebe2d1ede86b9f28f1e63b3e78e7e44a8
Meilandri <meilandriaditya@gmail.com>
2026-09-04 11:11:04 +0700

    database

--- config/database.php
+++ config/database.php
@@ -87,8 +87,8 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
+            'host' => env('DB_HOST', 'postgres.railway.internal'),
+            'port' => (int) env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
